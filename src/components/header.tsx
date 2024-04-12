3366e5a7d076748479505aa1fa40e579f147a2e4
szankacsanad <cs.szanka@gmail.com>
2024-04-12 10:40:49 +0200

    header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,24 @@
-import {} from "react-bootstrap";
+import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
-export default function Header(){
+export default function Header() {
     return (
-        <p>hel</p>
+        <header>
+            <Navbar expand="lg" className="bg-white mx-auto">
+            <Container className="mx-auto">
+                <Navbar.Brand href="#home">Használt Pegaut</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Szolgáltatásaink" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Autó vásárlás</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">Autó eladás</NavDropdown.Item>
+                        </NavDropdown>
+                        <Button variant="success">Hirdetésfeladás</Button>{' '}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar></header>
     )
-        
-    
+
+
 }
