3c7e75410a352c51a0a2781330994351856df532
notkissbe <bencetel1@gmail.com>
2024-04-12 10:41:48 +0200

    keres

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import {} from 'react-bootstrap';
+
+export default function Footer(){
+    return (
+        <footer>
+            <p>made by hmm</p>
+        </footer>
+    )
+}
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,10 +1,13 @@
 import { Container } from 'react-bootstrap';
 import Header from '../components/header';
+import Footer from '../components/footer';
+
 
 export default function Layout(){
     return(
         <Container fluid>
             <Header></Header>
+            <Footer></Footer>
         </Container>
     )
 }
