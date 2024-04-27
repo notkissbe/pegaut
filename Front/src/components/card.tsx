9fb3911a0dee1edbc6264f4b6e0db7a9b920b6e7
Benedek Schlesser <benedek.schlesser@gmail.com>
2024-04-27 21:21:33 +0200

    landing, cards -- fiat automobile de punto diesel

--- Front/src/components/card.tsx
+++ Front/src/components/card.tsx
@@ -2,7 +2,7 @@ import {} from 'react';
 import { Button, Card } from 'react-bootstrap'
 
 export interface PeugeotModel{
-    keplink:string;
+    kepLink:string;
     Model:string;
     Type:string;
     Year:number;
@@ -14,10 +14,10 @@ export interface PeugeotModel{
 export default function CreateCard( props:PeugeotModel){
     return (
         <Card className='m-auto' style={{ width: '18rem' }}>
-            <Card.Img variant='top' src='vite.svg'></Card.Img>
+            <Card.Img variant='top' src={`http://localhost:3000/${props.kepLink}`} style={{height: "15rem", objectFit: "cover", objectPosition: "100% 0"}}></Card.Img>
             <Card.Body>
                 <Card.Title>{props.Model}</Card.Title>
-                <Card.Text>{'lorem ipsum de pugot'}</Card.Text>
+                <Card.Text>{props.Price} Ft</Card.Text>
                 <Button variant='primary'>Megtekintés</Button>
             </Card.Body>
         </Card>
--- Front/src/pages/landing.tsx
+++ Front/src/pages/landing.tsx
@@ -18,7 +18,7 @@ export default function Landing() {
         <Container>
             <Row>
                 {
-                    models.map(oneModel => <CreateCard keplink={oneModel.keplink} Model={oneModel.Model} Type={oneModel.Type} Year={oneModel.Year} Price={oneModel.Price} EngineType={oneModel.EngineType} RangeDistance={oneModel.RangeDistance}/>)
+                    models.map(oneModel => <CreateCard kepLink={oneModel.kepLink} Model={oneModel.Model} Type={oneModel.Type} Year={oneModel.Year} Price={oneModel.Price} EngineType={oneModel.EngineType} RangeDistance={oneModel.RangeDistance}/>)
                 }
                             </Row>
         </Container>
