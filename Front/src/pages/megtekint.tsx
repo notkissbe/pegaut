a22f7b9eed9b89a9e31c4d4da9814796650ee297
Benedek Schlesser <benedek.schlesser@gmail.com>
2024-05-01 14:24:44 +0200

    megtekint --> hozzáadva

---
+++ Front/src/pages/megtekint.tsx
@@ -0,0 +1,25 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { PeugeotModel } from '../components/card';
+import { ListGroup } from 'react-bootstrap';
+
+export default function Megtekint(props:PeugeotModel) {
+    return(
+        <Card className='border rounded p-3 primary'>
+            <Card.Img variant='top' src={`http://localhost:3000/${props.kepLink}`}/>
+            <Card.Body>
+                <Card.Title>{props.Model}</Card.Title>
+            </Card.Body>
+            <ListGroup>
+                <ListGroup.Item>{props.Type}</ListGroup.Item>
+                <ListGroup.Item>{props.Year}</ListGroup.Item>
+                <ListGroup.Item>{props.Price}</ListGroup.Item>
+                <ListGroup.Item>{props.EngineType}</ListGroup.Item>
+                <ListGroup.Item>{props.RangeDistance}</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+                <Button variant='danger'>Feltöltés</Button>
+            </Card.Body>
+        </Card>
+    )
+}
