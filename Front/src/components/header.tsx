922edf6f00051e8f2e336127e924cf472e3b322f
Benedek Schlesser <benedek.schlesser@gmail.com>
2024-05-20 13:13:04 +0200

    search törölve

--- Front/src/components/header.tsx
+++ Front/src/components/header.tsx
@@ -3,17 +3,9 @@ import './header.css';
 import React, {Fragment, useState} from "react";
 import  ReactDOM from "react-dom";
 import {Transition} from "react-transition-group";
-import Sidebar from "./Searching/sidebar";
 import "./Searching/styles.css";
-import SortPriceD from "./sort"
 
 export default function Header() {
-    const [isOpen, toggleIsOpen] = useState<boolean>(false);
-    const onOpenModalClick = () => toggleIsOpen(true);
-    const onCloseModalClick = () => toggleIsOpen(false);
-    const duration: number = 250;
-
-
     
     function HandleSortSelection(mod:string){
         localStorage.setItem('SortBy',JSON.stringify(mod));
@@ -26,10 +18,7 @@ export default function Header() {
                 <Navbar.Brand href="/" id="cimSzoveg" className="text-light">Használt <img id="logo" src="../pegot.png"></img> Pegaut</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Navbar.Brand>
-                        <Button onClick={onOpenModalClick} variant="dark" className="btn-outline-light">Keresés</Button>
-                        </Navbar.Brand> 
+                    <Nav className="me-auto"> 
                         <NavDropdown title="Rendezés" id="basic-nav-dropdown" className="d-flex align-items-center">
                             <NavDropdown.Item onClick={()=>{HandleSortSelection("ArC")}}>Ár szerint csökkenő</NavDropdown.Item>
                             <NavDropdown.Item onClick={()=>{HandleSortSelection("ArN")}}>Ár szerint növekvő</NavDropdown.Item>
@@ -43,34 +32,7 @@ export default function Header() {
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-        <Fragment> 
-            <Transition in={isOpen} timeout={duration}>
-                {(state: string) => (
-                <Sidebar duration={duration} state={state}>
-                    {isOpen && (
-                        <div>
-                            <p></p>
-                            <h3>Model</h3>
-                            <input type="text" placeholder="Model"></input>
-                            <h3>Kivitel</h3>
-                            <input type="text" placeholder="Kivitel"></input>
-                            <h3>Évjárat</h3>
-                            <input type="number" min={1889} max={2024} placeholder="Évjárat"></input>
-                            <h3>Ár</h3>
-                            <input type="number" placeholder="Ár"></input>
-                            <h3>Motor</h3>
-                            <input type="text" placeholder="Motor"></input>
-                            <h3>Km</h3>
-                            <input type="number" placeholder="Km"></input>
-                            <p></p>
-                            <button onClick={onCloseModalClick} className="btn btn-dark">Bezárás</button>
-                            <p></p>
-                        </div>
-                    )}
-                </Sidebar>
-                )}
-            </Transition>
-        </Fragment></header>
+    </header>
     )
 
 
--- Front/src/components/sort.tsx
+++
@@ -1,5 +0,0 @@
-import { PeugeotModel } from "./card";
-
-export default function SortPriceD(models: PeugeotModel[]): PeugeotModel[] {
-    return [...models].sort((a, b) => a.Price - b.Price);
-}
